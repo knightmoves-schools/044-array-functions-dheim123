869dbd23c8de249cb79af1228d210d4add6938cd
dheim123 <dheim@midland.k12.ia.us>
2025-10-22 10:44:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+var automobiles = ['car', 'truck', 'motorcycle'];
+automobiles.push('airplane', 'airplane');
+automobiles.unshift('bike', 'helicopter');
+var automobilesCopy = automobiles;
+automobiles.pop();
+automobiles.shift();
+
+document.getElementById("result").innerHTML = automobiles;
 
